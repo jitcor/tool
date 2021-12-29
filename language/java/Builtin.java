8d0fef27621088260d4713222ae448567fe1f12e
Humenger <27600008+Humenger@users.noreply.github.com>
2021-12-29 11:14:17 +0800

    Update Builtin.java

--- language/java/Builtin.java
+++ language/java/Builtin.java
@@ -1,9 +1,9 @@
 public class Builtin {
-    @SafeVarargs
-    public static <T> T[] append(T[] slice, T... elems){
-        Object newElems[]=new Object[slice.length+elems.length];
+    public static byte[] append(byte[] slice, byte... elems){
+        byte[] newElems =new byte[slice.length+elems.length];
         System.arraycopy(slice,0,newElems,0,slice.length);
         System.arraycopy(elems,0,newElems,slice.length,elems.length);
-        return (T[])newElems;
+        return newElems;
     }
+
 }
