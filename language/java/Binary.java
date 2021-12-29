28221effe204cdc7aa37b97ff4ed1fb0b608abdd
Humenger <27600008+Humenger@users.noreply.github.com>
2021-12-29 11:16:00 +0800

    Update Binary.java

--- language/java/Binary.java
+++ language/java/Binary.java
@@ -8,6 +8,27 @@ public class Binary {
         public static int asInt32(byte[] b){
             return ((int)b[0])| ((int)b[1])<<8| ((int)b[2])<<16| ((int)b[3])<<24;
         }
+        public static byte[] asBytes(int v){
+            byte[] b=new byte[4];
+            b[0]= (byte) v;
+            b[1]= (byte) (v>>8);
+            b[2]= (byte) (v>>16);
+            b[3]= (byte) (v>>24);
+            return b;
+        }
+        public static byte[] asBytes(long v){
+            byte[] b=new byte[8];
+            b[0]= (byte) v;
+            b[1]= (byte) (v>>8);
+            b[2]= (byte) (v>>16);
+            b[3]= (byte) (v>>24);
+            b[4]= (byte) (v>>32);
+            b[5]= (byte) (v>>40);
+            b[6]= (byte) (v>>48);
+            b[7]= (byte) (v>>56);
+            return b;
+        }
+
         public static void putInt32(byte[] b,int v){
             b[0]= (byte) v;
             b[1]= (byte) (v>>8);
@@ -49,5 +70,26 @@ public class Binary {
             b[1]= (byte) (v>>48);
             b[0]= (byte) (v>>56);
         }
+        public static byte[] asBytes(int v){
+            byte[] b=new byte[4];
+            b[3]= (byte) v;
+            b[2]= (byte) (v>>8);
+            b[1]= (byte) (v>>16);
+            b[0]= (byte) (v>>24);
+            return b;
+        }
+        public static byte[] asBytes(long v){
+            byte[] b=new byte[8];
+            b[7]= (byte) v;
+            b[6]= (byte) (v>>8);
+            b[5]= (byte) (v>>16);
+            b[4]= (byte) (v>>24);
+            b[3]= (byte) (v>>32);
+            b[2]= (byte) (v>>40);
+            b[1]= (byte) (v>>48);
+            b[0]= (byte) (v>>56);
+            return b;
+        }
+
     }
 }
