ffe5583cf9a9a88512f9a505e6fdf5f387c40556
Humenger <27600008+Humenger@users.noreply.github.com>
2022-02-28 16:07:47 +0800

    Update Android.mk

--- Android/ndk/minimal_project/jni/Android.mk
+++ Android/ndk/minimal_project/jni/Android.mk
@@ -2,4 +2,5 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := minimal_project
 LOCAL_SRC_FILES := main.cpp
-include $(BUILD_EXECUTABLE)
+# include $(BUILD_EXECUTABLE)
+include $(BUILD_SHARED_LIBRARY)
