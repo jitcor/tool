040cc1a8cf434813a09e11b478a4640a5b1c728c
Humenger <27600008+Humenger@users.noreply.github.com>
2022-02-28 15:49:25 +0800

    Create main.cpp

---
+++ Android/ndk/minimal_project/jni/main.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+
+int main(int n_argc, char** argv)
+{
+	int n_num = n_argc * 2;
+	//scanf("%2d", &n_num);
+
+	if (20 == n_num)
+	{
+		puts("20");
+	}
+	if(10 == n_num)
+	{
+	  	puts("10");
+	}
+	if(2 == n_num)
+	{
+	  	puts("2");
+	}
+
+	puts("error");
+
+	return -1;
+}
