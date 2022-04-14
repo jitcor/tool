dfcdb3bc77baa4f113bc7059dbc4e7a9ff98e9c3
Humenger <louis4china@gmail.com>
2022-04-14 12:43:27 +0800

    Update only_screen_off.js

--- frida/android/only_screen_off.js
+++ frida/android/only_screen_off.js
@@ -3,5 +3,5 @@ testCode(true,function (){
    // < android Q 
    const SurfaceControl=Java.use('android.view.SurfaceControl');
    var token =SurfaceControl.getBuiltInDisplay(0);
-   SurfaceControl.setDisplayPowerMode(token,0);
+   SurfaceControl.setDisplayPowerMode(token,0);//0:off,2:no
 })
