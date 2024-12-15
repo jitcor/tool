a70d0b07112ca1f19065df27f07bcb1ca7b33bfe
shlu <shilulu@forensix.cn>
2024-12-15 12:10:40 +0800

    test(jekyll): 001

--- _plugins/markdown_link_replacer.rb
+++ _plugins/markdown_link_replacer.rb
@@ -7,7 +7,10 @@ module Jekyll
 
     # 指定该转换器处理的文件扩展名
     def matches(ext)
-      ext.downcase == '.md' || ext.downcase == '.markdown'
+      if ext != nil
+        ext.downcase == '.md' || ext.downcase == '.markdown'
+      end
+      false
     end
 
     # 保持文件扩展名不变
@@ -17,9 +20,11 @@ module Jekyll
 
     # 执行字符串替换并调用默认的 Markdown 转换器
     def convert(content)
+
       # 定义需要替换的字符串对，支持字符串和正则表达式
       replacements = [
-        { pattern: /\[([^\]]+)\]\(([^)]+)\.md\)/, replacement: '[\1](\2)' },
+        # { pattern: /\[([^\]]+)\]\(([^)]+)\.md\)/, replacement: '[\1](\2)' },
+        # { pattern: /\[([^\]]+)\]\(([^)]+)\.md\)/, replacement: '[\1](\2)' },
 #         { pattern: /旧字符串\d+/, replacement: '通用新字符串' }, # 使用正则表达式匹配“旧字符串”后跟一个或多个数字
         # 继续添加更多的替换规则
       ]
